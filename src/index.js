94d0e862bb59c04e2f387d6413ca79aae6dfe22f
Pedro Martins <ace@Pedros-MacBook-Pro.local>
2019-01-22 21:20:49 -0200

    - update static folder for old site for temporary placeholder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
+var path = require('path');
+var public = path.join(__dirname, 'html');
 const port = process.env.VIRTUAL_PORT || 3000;
 
 // app.get('/', (req, res) => res.send('Hello Pixelkiller World!'))
 
-app.use(express.static('html'))
+app.use(express.static(public))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
